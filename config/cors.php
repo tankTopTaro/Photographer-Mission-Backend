9d3f5b053e80ac1afb3bdae2886c4295e004f2bd
tankTopTaro <balmoresk3v1n@gmail.com>
2024-10-18 19:38:46 +0800

    deploying

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['*'],
+    'allowed_origins' => [env('FRONTEND_URL'), env('ENTRY_URL'), env('ADMIN_URL'), '*'],
 
     'allowed_origins_patterns' => [],
 
